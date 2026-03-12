01008c6c09d7b1f2ecb97271590a114bddcfa365
Arnav Anilkumar <arnav.anilkumar28@gmail.com>
2026-03-11 19:43:55 -0500

    Initial Commit

---
+++ MyBattery/MyBatteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBatteryApp.swift
+//  MyBattery
+//
+//  Created by Arnav Anilkumar on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct MyBatteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
